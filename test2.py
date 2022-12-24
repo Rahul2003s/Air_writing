403d6d79493997660697a6bd15fa42ad634a6919
Rahul.S <rahulsri073@gmail.com>
2022-12-24 15:05:43 +0000

    motion tap sence

---
+++ test2.py
@@ -0,0 +1,17 @@
+import time
+import board
+import busio
+import adafruit_adxl34x
+#(0.0784532, -0.1176798, 8.8652116)
+i2c=busio.I2C(board.SCL, board.SDA)
+ac=adafruit_adxl34x.ADXL345(i2c)
+ac.enable_motion_detection(threshold=18)
+ac.enable_tap_detection(tap_count=1,threshold=20,duration=50,latency=20,window=255)
+while True:
+#	print("tap: ",ac.events['tap'])
+#	print("motion:",ac.events['motion'])
+#	print("acceleration: ",ac.acceleration)
+	if ac.events['motion']:
+		print("acceleration: %f %f %f "%ac.acceleration)
+	time.sleep(0.5)
+

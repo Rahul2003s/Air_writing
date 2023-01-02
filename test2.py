9c38d6e3a953850a26a7186f0a316fa461e6c009
Rahul S <rahuls@Rahuls-MacBook-Air.local>
2023-01-02 19:16:45 +0530

    init

--- test2.py
+++ test2.py
@@ -6,12 +6,13 @@ import adafruit_adxl34x
 i2c=busio.I2C(board.SCL, board.SDA)
 ac=adafruit_adxl34x.ADXL345(i2c)
 ac.enable_motion_detection(threshold=18)
-ac.enable_tap_detection(tap_count=1,threshold=20,duration=50,latency=20,window=255)
+ac.enable_tap_detection(tap_count=3,threshold=20,duration=50,latency=20,window=255)
 while True:
 #	print("tap: ",ac.events['tap'])
 #	print("motion:",ac.events['motion'])
 #	print("acceleration: ",ac.acceleration)
-	if ac.events['motion']:
+	if ac.events['tap']:
+     	print("Reading input for 3 sec!!..")
 		print("acceleration: %f %f %f "%ac.acceleration)
 	time.sleep(0.5)
 

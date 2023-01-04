f1f5be09bdb246687b7cf2360cd012cc0eb9c19e
Rahul S <rahulsri@gmail.com>
2023-01-04 15:40:30 +0530

    input for 3 sec and writing in data.csv

--- test2.py
+++ test2.py
@@ -6,21 +6,28 @@ import adafruit_adxl34x
 #(0.0784532, -0.1176798, 8.8652116) 
 
 
-def write_data(cordinates,number):
+def write_data(cordinates,number,data_csv):
     x,y,z=cordinates
-    return x
+    l=list(cordinates)
+    data_csv.writerows(l)
 if __name__ == '__main__':    
     i2c=busio.I2C(board.SCL, board.SDA)
     ac=adafruit_adxl34x.ADXL345(i2c)
     ac.enable_motion_detection(threshold=18)
     ac.enable_tap_detection(tap_count=2,threshold=20,duration=50,latency=20,window=255)
+    
+    csv_file=open('data.csv','w')
+    data_csv=csv.writer(csv_file)
+    header=['x-axis','y-axis','z-axis','number']
+    data_csv.writeheader(header)
+    
     while True:
         if ac.events['tap']:
             print("Reading input for 3 sec!!..")
-            x,y,z=ac.acceleration
             t_end = time.time() + 2 
             count=0
             while time.time() < t_end: #records input for 2 seconds
-                print(x,y,z)
+                print(ac.acceleration)
+                write_data(ac.acceleration,n,data_csv)
                 count += 1
             print("Done",count)

95c505a07c6807af756f302e4825eeb73ff73bd8
Rahul S <rahulsri@gmail.com>
2023-01-15 00:39:49 +0530

    removing duplicate points

--- test2.py
+++ test2.py
@@ -18,9 +18,10 @@ def read_data(cordinates,number,list):
     
 def write_data(l):
     n=len(l)
-    for i in range(n):
-        if(l[i-1]['x-axis']==l[i]['x-axis']):
-            print(l[i]['x-axis'])
+    for i in range(1,n):
+        if((l[i-1]['x-axis']==l[i]['x-axis']) and (l[i-1]['y-axis']==l[i]['y-axis']) and (l[i-1]['z-axis']==l[i]['z-axis'])):
+            print(l[i]['x-axis'],l[i]['y-axis'],l[i]['z-axis'])
+            del l[i]
     
 if __name__ == '__main__':    
     i2c=busio.I2C(board.SCL, board.SDA)

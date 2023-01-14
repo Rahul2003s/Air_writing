7d2f4e1dd82a82ae3fa4f87092f0e6b5138391df
Rahul S <rahulsri@gmail.com>
2023-01-15 01:12:49 +0530

    removing duplicate points

--- test2.py
+++ test2.py
@@ -20,8 +20,9 @@ def write_data(l):
     n=len(l)
     print("LEN:",n)
     for i in range(1,n):
+        print(i)
         if((l[i-1]['x-axis']==l[i]['x-axis']) and (l[i-1]['y-axis']==l[i]['y-axis']) and (l[i-1]['z-axis']==l[i]['z-axis'])):
-            print(i)
+            # print(i)
             del l[i]
     
 if __name__ == '__main__':    

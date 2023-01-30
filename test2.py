beacebe8d1ee98a8a493e1122339c8ee344a2478
Rahul S <rahulsri@gmail.com>
2023-01-30 20:06:26 +0530

    x*1000

--- test2.py
+++ test2.py
@@ -9,9 +9,9 @@ import adafruit_adxl34x
 def read_data(cordinates,number,li):
     x,y,z=cordinates
     d=dict()
-    d['x-axis']= "%0.7f" % x
-    d['y-axis']= "%0.7f" % y
-    d['z-axis']= "%0.7f" % z 
+    d['x-axis']= x*1000
+    d['y-axis']= y*1000
+    d['z-axis']= z*1000 
     d['number']=number
     li.append(d)
     

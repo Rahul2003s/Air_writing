315c934c5ee1e4da5643fe7affbc161dc6ee2f80
Rahul S <rahulsri@gmail.com>
2023-04-02 02:12:25 +0530

    l

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ import busio
 import adafruit_adxl34x
 import math
 
-def read_data(cordinates,li):
+def read_data(cordinates,number,li):
     x,y,z=cordinates
     d=dict()
     xx="%0.7f" % x
@@ -16,6 +16,7 @@ def read_data(cordinates,li):
     d['x-axis']=float(xx)
     d['y-axis']=float(yy)
     d['z-axis']=float(zz)
+    d['number']=number
     li.append(d)
 
 
@@ -46,7 +47,7 @@ if __name__ == '__main__':
             t_end = time.time() + 2
             c=c+1
             while time.time() < t_end: #records input for 1.5 seconds
-                read_data(ac.acceleration,data_points)
+                read_data(ac.acceleration,n,data_points)
             if(c>1):
                 break
     print(data_points)
